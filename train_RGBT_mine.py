5eb8f498241fb239929570eee667d01f3e98d7c7
wandahangFY <wandahang@mail.hfut.edu.cn>
2025-02-20 00:21:05 +0800

    添加了YOLOv3 的yaml文件

--- train_RGBT_mine.py
+++ train_RGBT_mine.py
@@ -3,9 +3,9 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/v3-RGBT/yolov3-RGBT-scorefusion.yaml')
+    model = YOLO('ultralytics/cfg/models/11/yolo11.yaml')
     # model.load('yolov8n.pt') # loading pretrain weights
-    model.train(data=R'ultralytics/cfg/datasets/LLVIP_r20.yaml',
+    model.train(data=R'ultralytics/cfg/datasets/M3FD.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=300,
@@ -17,8 +17,8 @@ if __name__ == '__main__':
                 # resume='', # last.pt path
                 # amp=False, # close amp
                 # fraction=0.2,
-                use_simotm="RGBT",
-                channels=4,
-                project='runs/LLVIP_r20',
-                name='LLVIP_r20-yolov3-RGBT-scorefusion',
+                use_simotm="RGB",
+                channels=3,
+                project='runs/M3FD',
+                name='M3FD-yolo11n-RGB',
                 )
