9a8be37c8e69b5dd4da79bf26aef032b5eb71a2b
wandahangFY <wandahang@mail.hfut.edu.cn>
2025-02-20 20:38:43 +0800

    临时提交

--- train_RGBT_mine.py
+++ train_RGBT_mine.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/11/yolo11.yaml')
+    model = YOLO('ultralytics/cfg/models/11-RGBT/yolo11-RGBT-latefusion.yaml')
     # model.load('yolov8n.pt') # loading pretrain weights
     model.train(data=R'ultralytics/cfg/datasets/M3FD.yaml',
                 cache=False,
@@ -17,8 +17,8 @@ if __name__ == '__main__':
                 # resume='', # last.pt path
                 # amp=False, # close amp
                 # fraction=0.2,
-                use_simotm="RGB",
-                channels=3,
+                use_simotm="RGBT",
+                channels=4,
                 project='runs/M3FD',
-                name='M3FD-yolo11n-RGB',
+                name='M3FD-yolo11-RGBT-latefusion',
                 )
