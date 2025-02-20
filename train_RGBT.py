a8e504c0c7eb01ee8c9418e591a12e40614fcbeb
wandahangFY <wandahang@mail.hfut.edu.cn>
2025-02-20 23:03:37 +0800

    1.Added the YAML code for YOLOv9-RGBT. 2. Fixed some errors present in YOLOv10-RGBT.   1. 添加了YOLOv9-RGBT 的yaml代码
    2. 更改了YOLOv10中存在的一些错误

--- train_RGBT.py
+++ train_RGBT.py
@@ -3,13 +3,13 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/v9-RGBT/yolov9s-RGBT-scorefusion.yaml')
+    model = YOLO('ultralytics/cfg/models/v3-RGBT/yolov3-RGBT-scorefusion.yaml')
     # model.load('yolov8n.pt') # loading pretrain weights
     model.train(data=R'ultralytics/cfg/datasets/LLVIP_r20.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=10,
-                batch=16,
+                epochs=300,
+                batch=32,
                 close_mosaic=0,
                 workers=2,
                 device='0',
@@ -20,5 +20,5 @@ if __name__ == '__main__':
                 use_simotm="RGBT",
                 channels=4,
                 project='runs/LLVIP_r20',
-                name='LLVIP_r20-yolov9s-RGBT-scorefusion',
+                name='LLVIP_r20-yolov3-RGBT-scorefusion',
                 )
