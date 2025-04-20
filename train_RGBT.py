920b1b8951d98f069d91492b7169715409011e3a
wandahangFY <wandahang@mail.hfut.edu.cn>
2025-04-20 22:44:42 +0800

    2025-04-20   更新了引用说明 和6通道的默认ch

--- train_RGBT.py
+++ train_RGBT.py
@@ -3,7 +3,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/11-RGBT/yolo11n-RGBT-midfusion-CFT3_add.yaml')
+    model = YOLO('ultralytics/cfg/models/11-RGBT/yolo11n-RGBT-midfusion-CAS.yaml')
     # model.info(True,True)
     # model.load('yolov8n.pt') # loading pretrain weights
     model.train(data=R'ultralytics/cfg/datasets/LLVIP_r20.yaml',
@@ -15,11 +15,12 @@ if __name__ == '__main__':
                 workers=2,
                 device='0',
                 optimizer='SGD',  # using SGD
+                # lr0=0.002,
                 # resume='', # last.pt path
                 # amp=False, # close amp
                 # fraction=0.2,
                 use_simotm="RGBT",
                 channels=4,
                 project='runs/LLVIP_r20',
-                name='LLVIP_r20-yolo11n-RGBT-midfusion-CFT3_add-',
+                name='LLVIP_r20-yolo11n-RGBT-midfusion-CAS-',
                 )
